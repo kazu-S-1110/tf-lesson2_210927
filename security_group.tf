70089de6c4a6b661dcddb0ad82594a5380748b52
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-09-30 08:10:30 +0900

    webServerのsgの設定方法を学習。

---
+++ security_group.tf
@@ -0,0 +1,42 @@
+# -----------------------------------
+# security group
+# -----------------------------------
+# web server group
+resource "aws_security_group" "web_sg" {
+  name        = "${var.project}-${var.enviroment}-web-sg"
+  description = "web front role security group"
+  vpc_id      = aws_vpc.vpc.id
+
+  tags = {
+    Name    = "${var.project}-${var.enviroment}-web-sg"
+    Project = var.project
+    Env     = var.enviroment
+  }
+}
+
+resource "aws_security_group_rule" "web_in_http" {
+  security_group_id = aws_security_group.web_sg.id
+  type              = "ingress"
+  protocol          = "tcp"
+  from_port         = 80
+  to_port           = 80
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "web_in_https" {
+  security_group_id = aws_security_group.web_sg.id
+  type              = "ingress"
+  protocol          = "tcp"
+  from_port         = 443
+  to_port           = 443
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+resource "aws_security_group_rule" "web_out_tcp3000" {
+  security_group_id = aws_security_group.web_sg.id
+  type              = "egress"
+  protocol          = "tcp"
+  from_port         = 3000
+  to_port           = 3000
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
