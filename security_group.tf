bcf6f5735d05a8bf1a71937da2044ee8e06dc969
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-09-30 08:17:11 +0900

    apServerのsgを定義。

--- security_group.tf
+++ security_group.tf
@@ -40,3 +40,61 @@ resource "aws_security_group_rule" "web_out_tcp3000" {
   cidr_blocks       = ["0.0.0.0/0"]
 }
 
+# ap server group
+resource "aws_security_group" "app_sg" {
+  name        = "${var.project}-${var.enviroment}-app-sg"
+  description = "application server role security group"
+  vpc_id      = aws_vpc.vpc.id
+
+  tags = {
+    Name    = "${var.project}-${var.enviroment}-app-sg"
+    Project = var.project
+    Env     = var.enviroment
+  }
+}
+
+#opmng security group (運用管理用)
+resource "aws_security_group" "opmng_sg" {
+  name        = "${var.project}-${var.enviroment}-opmng-sg"
+  description = "operation and management role security group"
+  vpc_id      = aws_vpc.vpc.id
+
+  tags = {
+    Name    = "${var.project}-${var.enviroment}-opmng-sg"
+    Project = var.project
+    Env     = var.enviroment
+  }
+}
+
+resource "aws_security_group_rule" "opmng_in_ssh" {
+  security_group_id = aws_security_group.opmng_sg.id
+  type              = "ingress"
+  protocol          = "tcp"
+  from_port         = 22
+  to_port           = 22
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+resource "aws_security_group_rule" "opmng_in_tcp3000" {
+  security_group_id = aws_security_group.opmng_sg.id
+  type              = "ingress"
+  protocol          = "tcp"
+  from_port         = 3000
+  to_port           = 3000
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+resource "aws_security_group_rule" "opmng_out_http" {
+  security_group_id = aws_security_group.opmng_sg.id
+  type              = "egress"
+  protocol          = "tcp"
+  from_port         = 80
+  to_port           = 80
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+resource "aws_security_group_rule" "opmng_out_https" {
+  security_group_id = aws_security_group.opmng_sg.id
+  type              = "egress"
+  protocol          = "tcp"
+  from_port         = 443
+  to_port           = 443
+  cidr_blocks       = ["0.0.0.0/0"]
+}
