8cd0c89daecb5b62cae7c60d63caee26ad74cf98
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-09-29 21:31:44 +0900

    subnetを作成。

--- network.tf
+++ network.tf
@@ -11,3 +11,58 @@ resource "aws_vpc" "vpc" {
     Env     = var.enviroment
   }
 }
+
+resource "aws_subnet" "public_subnet_1a" {
+  vpc_id                  = aws_vpc.vpc.id
+  availability_zone       = "ap-northeast-1a"
+  cidr_block              = "192.168.1.0/24"
+  map_public_ip_on_launch = true //中に作成するEC2に自動でpublic ipを付与する
+
+  tags = {
+    "Name"  = "${var.project}-${var.enviroment}-public_subnet_1a"
+    Project = var.project
+    Env     = var.enviroment
+    Type    = "public"
+  }
+}
+resource "aws_subnet" "public_subnet_1c" {
+  vpc_id                  = aws_vpc.vpc.id
+  availability_zone       = "ap-northeast-1c"
+  cidr_block              = "192.168.2.0/24"
+  map_public_ip_on_launch = true //中に作成するEC2に自動でpublic ipを付与する
+
+  tags = {
+    "Name"  = "${var.project}-${var.enviroment}-public_subnet_1c"
+    Project = var.project
+    Env     = var.enviroment
+    Type    = "public"
+  }
+}
+resource "aws_subnet" "private_subnet_1a" {
+  vpc_id                  = aws_vpc.vpc.id
+  availability_zone       = "ap-northeast-1a"
+  cidr_block              = "192.168.3.0/24"
+  map_public_ip_on_launch = false //中に作成するEC2に自動でpublic ipを付与する
+
+  tags = {
+    "Name"  = "${var.project}-${var.enviroment}-private_subnet_1a"
+    Project = var.project
+    Env     = var.enviroment
+    Type    = "private"
+  }
+}
+resource "aws_subnet" "private_subnet_1c" {
+  vpc_id                  = aws_vpc.vpc.id
+  availability_zone       = "ap-northeast-1c"
+  cidr_block              = "192.168.4.0/24"
+  map_public_ip_on_launch = false //中に作成するEC2に自動でpublic ipを付与する
+
+  tags = {
+    "Name"  = "${var.project}-${var.enviroment}-private_subnet_1c"
+    Project = var.project
+    Env     = var.enviroment
+    Type    = "private"
+  }
+}
+
+
