e8114b44736ee93d6ad2ddaf3d1c6b209450b0e0
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-09-30 08:23:23 +0900

    dbServerのsgを定義。

--- security_group.tf
+++ security_group.tf
@@ -32,12 +32,12 @@ resource "aws_security_group_rule" "web_in_https" {
   cidr_blocks       = ["0.0.0.0/0"]
 }
 resource "aws_security_group_rule" "web_out_tcp3000" {
-  security_group_id = aws_security_group.web_sg.id
-  type              = "egress"
-  protocol          = "tcp"
-  from_port         = 3000
-  to_port           = 3000
-  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id        = aws_security_group.web_sg.id
+  type                     = "egress"
+  protocol                 = "tcp"
+  from_port                = 3000
+  to_port                  = 3000
+  source_security_group_id = aws_security_group.app_sg.id
 }
 
 # ap server group
@@ -97,4 +97,26 @@ resource "aws_security_group_rule" "opmng_out_https" {
   from_port         = 443
   to_port           = 443
   cidr_blocks       = ["0.0.0.0/0"]
+}
+
+#db server group
+resource "aws_security_group" "db_sg" {
+  name        = "${var.project}-${var.enviroment}-db-sg"
+  description = "database role security group"
+  vpc_id      = aws_vpc.vpc.id
+
+  tags = {
+    Name    = "${var.project}-${var.enviroment}-db-sg"
+    Project = var.project
+    Env     = var.enviroment
+  }
+}
+
+resource "aws_security_group_rule" "db_in_tcp3306" {
+  security_group_id        = aws_security_group.db_sg.id
+  type                     = "ingress"
+  protocol                 = "tcp"
+  from_port                = 3306
+  to_port                  = 3306
+  source_security_group_id = aws_security_group.app_sg.id
 }
