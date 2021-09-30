ef9f259af6cba9ca9515e2364e61225aaa200c46
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-10-01 08:14:24 +0900

    オプショングループを作成。

--- rds.tf
+++ rds.tf
@@ -2,16 +2,25 @@
 # RDS parameter group
 # -----------------------------------
 resource "aws_db_parameter_group" "mysql_standalone_parametergroup" {
-  name = "${var.project}-${var.enviroment}-mysql_standalone_parametergroup"
+  name   = "${var.project}-${var.enviroment}-mysql-standalone-parametergroup"
   family = "mysql8.0"
 
   parameter {
-    name = "character_set_database"
+    name  = "character_set_database"
     value = "utf8mb4"
   }
 
   parameter {
-    name = "character_set_server"
+    name  = "character_set_server"
     value = "utf8mb4"
   }
+}
+
+# -----------------------------------
+# option group
+# -----------------------------------
+resource "aws_db_option_group" "mysql_standalone_optiongroup" {
+  name                 = "${var.project}-${var.enviroment}-mysql-standalone-optiongroup"
+  engine_name          = "mysql"
+  major_engine_version = "8.0"
 }
