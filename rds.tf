aedbd8c18505d2575db137fa7726bcb5a5d6cf2e
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-10-01 08:22:58 +0900

    rdsのサプネットグループを作成。

--- rds.tf
+++ rds.tf
@@ -24,3 +24,20 @@ resource "aws_db_option_group" "mysql_standalone_optiongroup" {
   engine_name          = "mysql"
   major_engine_version = "8.0"
 }
+
+# -----------------------------------
+# subnet group
+# -----------------------------------
+resource "aws_db_subnet_group" "mysql_standalone_subnetgroup" {
+  name = "${var.project}-${var.enviroment}-mysql-standalone-subnetgroup"
+  # rdsが利用できるサブネットを指定する
+  subnet_ids = [
+    aws_subnet.private_subnet_1a.id,
+    aws_subnet.private_subnet_1c.id,
+  ]
+  tags = {
+    Name    = "${var.project}-${var.enviroment}-mysql-mysql_standalone_subnetgroup"
+    Project = var.project
+    Env     = var.enviroment
+  }
+}
