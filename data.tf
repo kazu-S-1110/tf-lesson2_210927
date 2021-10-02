767ed36111e4830deea0ac3e0a2a4f5d0c34c08e
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-10-03 00:14:15 +0900

    作成するec2のイメージを絞るようdataを定義。

--- data.tf
+++ data.tf
@@ -1,3 +1,21 @@
 data "aws_prefix_list" "s3_pl" {
   name = "com.amazonaws.*.s3"
 }
+
+data "aws_ami" "name" {
+  most_recent = true
+  owners      = ["self", "amazon"]
+
+  filter {
+    name  = "name"
+    value = ["amzn2-ami-hvm-2.0.*-x86_64-gp2"]
+  }
+  filter {
+    name  = "root-device-type"
+    value = ["ebs"]
+  }
+  filter {
+    name  = "virtualization-type"
+    value = ["hvm"]
+  }
+}
--- rds.tf
+++ rds.tf
@@ -50,54 +50,54 @@ resource "random_string" "db_password" {
   special = false
 }
 
-# resource "aws_db_instance" "mysql_standalone" {
-#   engine         = "mysql"
-#   engine_version = "8.0.20"
+resource "aws_db_instance" "mysql_standalone" {
+  engine         = "mysql"
+  engine_version = "8.0.20"
 
-#   identifier = "${var.project}-${var.enviroment}-mysql-standalone"
+  identifier = "${var.project}-${var.enviroment}-mysql-standalone"
 
-#   username = "admin"
-#   # パスワードには上で作成したrandom文字列を設定
-#   password = random_string.db_password.result
+  username = "admin"
+  # パスワードには上で作成したrandom文字列を設定
+  password = random_string.db_password.result
 
-#   instance_class = "db.t2.micro"
+  instance_class = "db.t2.micro"
 
-#   #storage setting
-#   allocated_storage     = 20
-#   max_allocated_storage = 30
-#   storage_type          = "gp2"
-#   storage_encrypted     = false
+  #storage setting
+  allocated_storage     = 20
+  max_allocated_storage = 30
+  storage_type          = "gp2"
+  storage_encrypted     = false
 
-#   #network setting
-#   multi_az               = false
-#   availability_zone      = "ap-northeast-1a"
-#   db_subnet_group_name   = aws_db_subnet_group.mysql_standalone_subnetgroup.name
-#   vpc_security_group_ids = [aws_security_group.db_sg.id]
-#   publicly_accessible    = false
-#   port                   = 3306
+  #network setting
+  multi_az               = false
+  availability_zone      = "ap-northeast-1a"
+  db_subnet_group_name   = aws_db_subnet_group.mysql_standalone_subnetgroup.name
+  vpc_security_group_ids = [aws_security_group.db_sg.id]
+  publicly_accessible    = false
+  port                   = 3306
 
-#   name                 = "tastylog"
-#   parameter_group_name = aws_db_parameter_group.mysql_standalone_parametergroup.name
-#   option_group_name    = aws_db_option_group.mysql_standalone_optiongroup.name
+  name                 = "tastylog"
+  parameter_group_name = aws_db_parameter_group.mysql_standalone_parametergroup.name
+  option_group_name    = aws_db_option_group.mysql_standalone_optiongroup.name
 
-#   #maintenance setting
-#   backup_window              = "04:00-05:00"
-#   backup_retention_period    = 7 #何日分保存するか
-#   maintenance_window         = "Mon:05:00-Mon:08:00"
-#   auto_minor_version_upgrade = false
+  #maintenance setting
+  backup_window              = "04:00-05:00"
+  backup_retention_period    = 7 #何日分保存するか
+  maintenance_window         = "Mon:05:00-Mon:08:00"
+  auto_minor_version_upgrade = false
 
-#   #deletion setting
-#   # deletion_protection = true
-#   # skip_final_snapshot = false
-#   # apply_immediately = true
-#   #削除する時には設定を変更させる必要がある
-#   deletion_protection = false
-#   skip_final_snapshot = true
-#   apply_immediately   = true
+  #deletion setting
+  deletion_protection = true
+  skip_final_snapshot = false
+  apply_immediately   = true
+  #削除する時には設定を変更させる必要がある
+  # deletion_protection = false
+  # skip_final_snapshot = true
+  # apply_immediately   = true
 
-#   tags = {
-#     "Name"  = "${var.project}-${var.enviroment}-mysql-standalone"
-#     Project = var.project
-#     Env     = var.enviroment
-#   }
-# }
+  tags = {
+    "Name"  = "${var.project}-${var.enviroment}-mysql-standalone"
+    Project = var.project
+    Env     = var.enviroment
+  }
+}
