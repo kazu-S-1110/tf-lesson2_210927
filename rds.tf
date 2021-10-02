eb251c88dbb6d25dd69c3bcd219b1fb2db3a6c0a
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-10-02 12:14:52 +0900

    rdsインスタンスの設定。

--- rds.tf
+++ rds.tf
@@ -49,3 +49,35 @@ resource "random_string" "db_password" {
   length  = 16
   special = false
 }
+
+resource "aws_db_instance" "mysql_standalone" {
+  engine         = "mysql"
+  engine_version = "8.0.20"
+
+  identifier = "${var.project}-${var.enviroment}-mysql-standalone"
+
+  username = "admin"
+  # パスワードには上で作成したrandom文字列を設定
+  password = random_string.db_password.result
+
+  #storage setting
+  allocated_storage     = 20
+  max_allocated_storage = 30
+  storage_type          = "gp2"
+  storage_encrypted     = false
+
+  #network setting
+  multi_az               = false
+  availability_zone      = "ap-northeast-1a"
+  db_subnet_group_name   = aws_db_subnet_group.mysql_standalone_subnetgroup.name
+  vpc_security_group_ids = [aws_security_group.db_sg.id]
+  publicly_accessible    = false
+  port                   = 3306
+
+  name                 = "tastylog"
+  parameter_group_name = aws_db_parameter_group.mysql_standalone_parametergroup.name
+  option_group_name    = aws_db_option_group.mysql_standalone_optiongroup.name
+
+
+
+}
