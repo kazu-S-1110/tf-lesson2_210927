c3d7eee85e8f8035f77df229e76116470fc20152
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-10-05 08:06:14 +0900

    policyをattachment。似たresourceがあって焦った。

--- iam.tf
+++ iam.tf
@@ -20,3 +20,21 @@ data "aws_iam_policy_document" "ec2_assume_role" {
     }
   }
 }
+
+# policy attachment(構文変わってる？)
+resource "aws_iam_role_policy_attachment" "app_iam_role_ec2_readonly" {
+  role       = aws_iam_role.app_iam_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ReadOnlyAccess"
+}
+resource "aws_iam_role_policy_attachment" "app_iam_role_ssm_managed" {
+  role       = aws_iam_role.app_iam_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+resource "aws_iam_role_policy_attachment" "app_iam_role_ssm_readonly" {
+  role       = aws_iam_role.app_iam_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMReadOnlyAccess"
+}
+resource "aws_iam_role_policy_attachment" "app_iam_role_s3_readonly" {
+  role       = aws_iam_role.app_iam_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+}
