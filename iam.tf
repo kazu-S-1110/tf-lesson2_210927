c44f21cdaa40e13390d11caa52b1e3d3f56da53d
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-10-04 20:03:59 +0900

    IAMロールを作成。

--- iam.tf
+++ iam.tf
@@ -1,3 +1,14 @@
+# -----------------------------------
+# IAM Role
+# -----------------------------------
+resource "aws_iam_role" "app_iam_role" {
+  name = "${var.project}-${var.enviroment}-app-iam-role"
+  #信頼ずみポリシーを指定
+  assume_role_policy = data.aws_iam_policy_document.ec2_assume_role.json
+}
+
+
+
 data "aws_iam_policy_document" "ec2_assume_role" {
   #自動で作成されるものを明示的に定義するのでおまじないだと思えばおけ。
   statement {
