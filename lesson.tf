084b01aaf92fa043d14d9aeeb5b99bdcca716cc7
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-10-06 08:06:14 +0900

    複数リソースを生成するfor_eachを学習。(補完効かないの、、、?

--- lesson.tf
+++ lesson.tf
@@ -3,3 +3,19 @@
 #   count = 5
 #   name  = "test-user-${count.index + 1}"
 # }
+
+# for-eachを使用してobjectライクに扱う方法
+# resource "aws_vpc" "test-vpc" {
+#   cidr_block = "192.168.0.0/20"
+# }
+# resource "aws_subnet" "test-subnet" {
+#   for_each = {
+#     "192.168.1.0/24" = "ap-northeast-1a"
+#     "192.168.2.0/24" = "ap-northeast-1c"
+#     "192.168.3.0/24" = "ap-northeast-1d"
+#   }
+
+#   vpc_id            = aws_vpc.test-vpc.id
+#   cidr_block        = each.key
+#   availability_zone = each.value
+# }
