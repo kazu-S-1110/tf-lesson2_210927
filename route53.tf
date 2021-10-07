824ae451bb5ffa4a683507ce134afb246d99a309
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-10-08 08:22:49 +0900

    route53のレコードを作成。（レコードがなんだか曖昧だった。

--- route53.tf
+++ route53.tf
@@ -2,12 +2,23 @@
 # route53
 # -----------------------------------
 resource "aws_route53_zone" "route53_zone" {
-  name = var.domain
+  name          = var.domain
   force_destroy = false
-  
+
   tags = {
     "Name"  = "${var.project}-${var.enviroment}-domain"
     Project = var.project
     Env     = var.enviroment
+  }
+}
+
+resource "aws_route53_record" "route53_record" {
+  zone_id = aws_route53_zone.route53_zone.id
+  name    = "dev-elb.${var.domain}"
+  type    = "A"
+  alias {
+    name                   = aws_alb.alb.dns_name
+    zone_id                = aws_alb.alb.zone_id
+    evaluate_target_health = true
   }
 }
