61a8a48f3dc5f663dea3c37a8ac7ac935147d961
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-10-10 21:16:24 +0900

    プライベートバケットを作成。

--- s3.tf
+++ s3.tf
@@ -45,3 +45,44 @@ data "aws_iam_policy_document" "s3_static_bucket" {
     }
   }
 }
+
+# ---------------------------------------------
+# s3 deploy bucket
+# ---------------------------------------------
+resource "aws_s3_bucket" "s3_deploy_bucket" {
+  bucket = "${var.project}-${var.environment}-deploy-bucket-${random_string.s3_unique_key.result}"
+
+  versioning {
+    enabled = false
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "s3_deploy_bucket" {
+  bucket                  = aws_s3_bucket.s3_deploy_bucket.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+
+  depends_on = [
+    aws_s3_bucket_policy.s3_deploy_bucket
+  ]
+}
+
+resource "aws_s3_bucket_policy" "s3_deploy_bucket" {
+  bucket = aws_s3_bucket.s3_deploy_bucket.id
+  policy = data.aws_iam_policy_document.s3_deploy_bucket.json
+}
+
+data "aws_iam_policy_document" "s3_deploy_bucket" {
+  statement {
+    effect    = "Allow"
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.s3_deploy_bucket.arn}/*"]
+    principals {
+      //誰からの通信を許可するか
+      type        = "AWS"
+      identifiers = [aws_iam_role.app_iam_role.arn]
+    }
+  }
+}
