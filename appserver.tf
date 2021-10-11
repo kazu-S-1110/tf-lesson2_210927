5a65940b37e0bf1c9f4ae47e3ef3870748974a2b
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-10-11 22:39:40 +0900

    起動テンプレートを作成。これでEC2が作れるのか

--- appserver.tf
+++ appserver.tf
@@ -15,28 +15,28 @@ resource "aws_key_pair" "keypair" {
 # -----------------------------------
 # ec2 instance
 # -----------------------------------
-resource "aws_instance" "app_server" {
-  # aws_amiで検索したものを指定する
-  ami                         = data.aws_ami.app.id
-  instance_type               = "t2.micro"
-  subnet_id                   = aws_subnet.public_subnet_1a.id
-  associate_public_ip_address = true
-  #作成したIAMロールを適用
-  iam_instance_profile = aws_iam_instance_profile.app_ec2_profile.name
-  vpc_security_group_ids = [
-    aws_security_group.app_sg.id,
-    aws_security_group.opmng_sg.id,
-  ]
-  # keypairの参照名はkey_nameなので注意。詳しくはリファレンス見てね。
-  key_name = aws_key_pair.keypair.key_name
+# resource "aws_instance" "app_server" {
+#   # aws_amiで検索したものを指定する
+#   ami                         = data.aws_ami.app.id
+#   instance_type               = "t2.micro"
+#   subnet_id                   = aws_subnet.public_subnet_1a.id
+#   associate_public_ip_address = true
+#   #作成したIAMロールを適用
+#   iam_instance_profile = aws_iam_instance_profile.app_ec2_profile.name
+#   vpc_security_group_ids = [
+#     aws_security_group.app_sg.id,
+#     aws_security_group.opmng_sg.id,
+#   ]
+#   # keypairの参照名はkey_nameなので注意。詳しくはリファレンス見てね。
+#   key_name = aws_key_pair.keypair.key_name
 
-  tags = {
-    "Name"  = "${var.project}-${var.environment}-app-ec2"
-    Project = var.project
-    Env     = var.environment
-    Type    = "app"
-  }
-}
+#   tags = {
+#     "Name"  = "${var.project}-${var.environment}-app-ec2"
+#     Project = var.project
+#     Env     = var.environment
+#     Type    = "app"
+#   }
+# }
 
 # aws_instanceの取り込み方法 (cmd:terraform import aws_instance.test [id])
 #もしtfstateをs3保存している場合にはterraform importの結果はs3へ反映される。
@@ -76,4 +76,44 @@ resource "aws_ssm_parameter" "password" {
   name  = "/${var.project}/${var.environment}/app/MYSQL_PASSWORD"
   type  = "SecureString"
   value = aws_db_instance.mysql_standalone.password
+}
+
+
+# ---------------------------------------------
+# launch template
+# ---------------------------------------------
+resource "aws_launch_template" "app_lt" {
+  update_default_version = true
+
+  name = "${var.project}-${var.environment}-app-lt"
+
+  image_id = data.aws_ami.app.id
+
+  key_name = aws_key_pair.keypair.key_name
+
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      "Name"  = "${var.project}-${var.environment}-app-ec2"
+      Project = var.project
+      Env     = var.environment
+      Type    = "app"
+    }
+  }
+
+  network_interfaces {
+    associate_public_ip_address = true
+    security_groups = [
+      aws_security_group.app_sg.id,
+      aws_security_group.opmng_sg.id,
+    ]
+    delete_on_termination = true
+  }
+
+  iam_instance_profile {
+    name = aws_iam_instance_profile.app_ec2_profile.name
+  }
+
+  # デプロイ用s3からダウンロードして立ち上げていく流れ（そのための初期化スクリプト）
+  user_data = filebase64("./src/initialize.sh")
 }
--- data.tf
+++ data.tf
@@ -8,14 +8,18 @@ data "aws_ami" "app" {
 
   filter {
     name   = "name"
-    values = ["amzn2-ami-hvm-2.0.*-x86_64-gp2"]
+    values = ["tastylog-app-ami"]
   }
-  filter {
-    name   = "root-device-type"
-    values = ["ebs"]
-  }
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
+  # filter {
+  #   name   = "name"
+  #   values = ["amzn2-ami-hvm-2.0.*-x86_64-gp2"]
+  # }
+  # filter {
+  #   name   = "root-device-type"
+  #   values = ["ebs"]
+  # }
+  # filter {
+  #   name   = "virtualization-type"
+  #   values = ["hvm"]
+  # }
 }
